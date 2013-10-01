151054db1ff0726cdc1b8feda190d837e6018d6f
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-10-01 14:21:57 +0200

    Started implementing polygon partitioning

---
+++ project/src/de/topobyte/polygon/monotonepieces/MonotonePiecesPanel.java
@@ -0,0 +1,69 @@
+/* This file is part of LiveCG. 
+ * 
+ * Copyright (C) 2013  Sebastian Kuerten
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.topobyte.polygon.monotonepieces;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Area;
+
+import javax.swing.JPanel;
+
+import de.topobyte.livecg.geometry.geom.AwtHelper;
+import de.topobyte.livecg.geometry.geom.Chain;
+import de.topobyte.livecg.geometry.geom.Coordinate;
+import de.topobyte.livecg.geometry.geom.Polygon;
+import de.topobyte.util.SwingUtil;
+
+public class MonotonePiecesPanel extends JPanel
+{
+
+	private static final long serialVersionUID = 2129465700417909129L;
+
+	private Polygon polygon;
+
+	public MonotonePiecesPanel(Polygon polygon)
+	{
+		this.polygon = polygon;
+	}
+
+	@Override
+	public void paint(Graphics graphics)
+	{
+		Graphics2D g = (Graphics2D) graphics;
+		SwingUtil.useAntialiasing(g, true);
+
+		Area shape = AwtHelper.toShape(polygon);
+		g.setColor(new Color(0x66ff0000, true));
+		g.fill(shape);
+
+		g.setColor(Color.BLACK);
+		Chain shell = polygon.getShell();
+		for (int i = 0; i < shell.getNumberOfNodes(); i++) {
+			Coordinate c1 = shell.getCoordinate(i);
+			int j = i + 1;
+			if (j == shell.getNumberOfNodes()) {
+				j = 0;
+			}
+			Coordinate c2 = shell.getCoordinate(j);
+			g.drawLine((int) Math.round(c1.getX()),
+					(int) Math.round(c1.getY()), (int) Math.round(c2.getX()),
+					(int) Math.round(c2.getY()));
+		}
+	}
+}
