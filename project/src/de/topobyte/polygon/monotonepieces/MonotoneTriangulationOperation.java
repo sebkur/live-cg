f6c584518b00b6f902257d3308b10a0ac75b800f
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-10-02 16:28:29 +0200

    Triangulation: Merging of left and right chains

--- project/src/de/topobyte/polygon/monotonepieces/MonotoneTriangulationOperation.java
+++ project/src/de/topobyte/polygon/monotonepieces/MonotoneTriangulationOperation.java
@@ -17,11 +17,15 @@
  */
 package de.topobyte.polygon.monotonepieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.topobyte.livecg.geometry.geom.Chain;
 import de.topobyte.livecg.geometry.geom.Coordinate;
+import de.topobyte.livecg.geometry.geom.IntRing;
 import de.topobyte.livecg.geometry.geom.Node;
 import de.topobyte.livecg.geometry.geom.Polygon;
 
@@ -37,6 +41,7 @@ public class MonotoneTriangulationOperation
 	{
 		this.polygon = polygon;
 
+		// Determine top and bottom nodes
 		double minY = Double.POSITIVE_INFINITY;
 		double maxY = Double.NEGATIVE_INFINITY;
 		int minIndex = -1;
@@ -56,5 +61,42 @@ public class MonotoneTriangulationOperation
 		}
 		logger.debug("Top node: #" + (minIndex + 1));
 		logger.debug("Bottom node: #" + (maxIndex + 1));
+
+		// Merge chains by traversing left and right chain
+		List<Node> nodes = new ArrayList<Node>();
+		nodes.add(shell.getNode(minIndex));
+		IntRing left = new IntRing(shell.getNumberOfNodes(), minIndex).next();
+		IntRing right = new IntRing(shell.getNumberOfNodes(), minIndex).prev();
+		while (left.value() != maxIndex || right.value() != maxIndex) {
+			if (left.value() == maxIndex) {
+				logger.debug("Left chain fished");
+				Coordinate c = shell.getCoordinate(right.value());
+				logger.debug("Add: " + (right.value() + 1) + ": " + c.getY());
+				right.prev();
+				continue;
+			} else if (right.value() == maxIndex) {
+				logger.debug("Right chain fished");
+				Coordinate c = shell.getCoordinate(left.value());
+				logger.debug("Add: " + (left.value() + 1) + ": " + c.getY());
+				left.next();
+				continue;
+			}
+			int l = left.value();
+			int r = right.value();
+			Node nl = shell.getNode(l);
+			Node nr = shell.getNode(r);
+			Coordinate cl = nl.getCoordinate();
+			Coordinate cr = nr.getCoordinate();
+			if (cl.getY() <= cr.getY()) {
+				Coordinate c = shell.getCoordinate(left.value());
+				logger.debug("Add: " + (left.value() + 1) + ": " + c.getY());
+				left.next();
+			} else {
+				Coordinate c = shell.getCoordinate(right.value());
+				logger.debug("Add: " + (right.value() + 1) + ": " + c.getY());
+				right.prev();
+			}
+		}
+		nodes.add(shell.getNode(maxIndex));
 	}
 }
