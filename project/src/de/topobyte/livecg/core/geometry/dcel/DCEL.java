33e6aa97d898a8b35ee570527a870893beea8230
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-10-18 20:59:29 +0200

    Added class for the DCEL itself

---
+++ project/src/de/topobyte/livecg/core/geometry/dcel/DCEL.java
@@ -0,0 +1,30 @@
+/* This file is part of LiveCG. 
+ * 
+ * Copyright (C) 2013  Sebastian Kuerten
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.topobyte.livecg.core.geometry.dcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DCEL
+{
+
+	public List<Vertex> vertices = new ArrayList<Vertex>();
+	public List<HalfEdge> halfedges = new ArrayList<HalfEdge>();
+	public List<Face> faces = new ArrayList<Face>();
+
+}
