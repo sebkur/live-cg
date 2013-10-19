153e78f31afa6af394ea162304eefd375f0bc846
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-10-19 17:13:37 +0200

    DCEL visualization has neat halfedge arrows

--- project/src/de/topobyte/livecg/algorithms/dcel/HalfEdgeArrow.java
+++ project/src/de/topobyte/livecg/algorithms/dcel/HalfEdgeArrow.java
@@ -20,37 +20,91 @@ package de.topobyte.livecg.algorithms.dcel;
 import de.topobyte.livecg.core.geometry.dcel.HalfEdge;
 import de.topobyte.livecg.core.geometry.dcel.Vertex;
 import de.topobyte.livecg.core.geometry.geom.Coordinate;
+import de.topobyte.livecg.core.geometry.geom.GeomMath;
 import de.topobyte.livecg.core.lina2.Vector;
 
 public class HalfEdgeArrow
 {
+	private double gap = 6;
+	private double shorten = 5;
+	private double length = 12;
+	private double alpha = Math.PI / 8;
+	private double lsa = length * Math.sin(alpha);
+	private double lca = length * Math.cos(alpha);
 
 	private Vector ao, ad, am;
 
 	public HalfEdgeArrow(HalfEdge halfedge)
 	{
-		HalfEdge twin = halfedge.getTwin();
 		Vertex origin = halfedge.getOrigin();
-		Vertex destination = twin.getOrigin();
+		Vertex destination = halfedge.getTwin().getOrigin();
 		Coordinate co = origin.getCoordinate();
 		Coordinate cd = destination.getCoordinate();
 
-		double gap = 5;
-		double shorten = 10;
-		double length = 15;
-		double alpha = Math.PI / 8;
-		double lsa = length * Math.sin(alpha);
-		double lca = length * Math.cos(alpha);
+		HalfEdge prev = halfedge.getPrev();
+		Vertex prevOrigin = prev.getOrigin();
+		Coordinate cpo = prevOrigin.getCoordinate();
 
-		Vector vo = new Vector(co);
-		Vector vd = new Vector(cd);
-		Vector he = new Vector(co, cd).normalized();
-		Vector ppd = he.perpendicular().normalized();
+		HalfEdge next = halfedge.getNext();
+		Vertex nextDestination = next.getTwin().getOrigin();
+		Coordinate cnd = nextDestination.getCoordinate();
 
-		ao = vo.add(ppd.mult(gap)).add(he.mult(shorten));
-		ad = vd.add(ppd.mult(gap)).sub(he.mult(shorten));
+		ao = findPoint(cpo, co, cd, co, cd, true);
+		ad = findPoint(co, cd, cnd, co, cd, false);
 
-		am = ad.add(ppd.mult(lsa)).sub(he.mult(lca));
+		Vector e = new Vector(co, cd).normalized();
+		Vector ppd = e.perpendicular().normalized();
+		am = ad.add(ppd.mult(lsa)).sub(e.mult(lca));
+	}
+
+	/**
+	 * Coordinates c1, c2, c3 form the chain to find a point for.
+	 * 
+	 * Coordinates co, cd are the coordinates of the segment that the current
+	 * arrow is parallel to.
+	 * 
+	 * @param isOrigin
+	 *            tells whether the method call is for a start- or endpoint of
+	 *            the arrow
+	 */
+	private Vector findPoint(Coordinate c1, Coordinate c2, Coordinate c3,
+			Coordinate co, Coordinate cd, boolean isOrigin)
+	{
+		Vector v2 = new Vector(c2);
+		Vector e1 = new Vector(c1, c2).normalized();
+		Vector e2 = new Vector(c2, c3).normalized();
+		Vector e = new Vector(co, cd).normalized();
+
+		double angle = GeomMath.angle(c2, c1, c3);
+		if (angle <= Math.PI) { // Acute angle
+			// d is the direction in which to shift the point from v2
+			Vector d = e1.mult(-1).add(e2).normalized();
+			// Find lambda such that lambda * d shifts the target point p from
+			// v2 with the property that p has distance 'gap' to e1 and e2
+			double dx2 = d.getX() * d.getX();
+			double ex2 = e2.getX() * e2.getX();
+			double dy2 = d.getY() * d.getY();
+			double ey2 = e2.getY() * e2.getY();
+			double remaining = 2 * d.getX() * e2.getX() * d.getY() * e2.getY();
+			double lambda = gap
+					/ Math.sqrt(1 - (dx2 * ex2 + dy2 * ey2 + remaining));
+			Vector p = v2.add(d.mult(lambda));
+			if (isOrigin) {
+				return p.add(e.mult(shorten));
+			} else {
+				return p.sub(e.mult(shorten));
+			}
+		} else { // Obtuse angle
+			// Just move the target point p an amount 'gap' in the direction
+			// perpendicular to the edge
+			Vector ppd = e.perpendicular().normalized();
+			Vector p = v2.add(ppd.mult(gap));
+			if (isOrigin) {
+				return p.add(e.mult(shorten));
+			} else {
+				return p.sub(e.mult(shorten));
+			}
+		}
 	}
 
 	public Vector getOrigin()
