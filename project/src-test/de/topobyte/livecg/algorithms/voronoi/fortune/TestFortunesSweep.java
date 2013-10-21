33c9c0026490a5df580b36e4bc0875ca3739555d
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-10-21 19:47:42 +0200

    TestFortuneSweep uses preset geometries

--- project/src-test/de/topobyte/livecg/algorithms/voronoi/fortune/TestFortunesSweep.java
+++ project/src-test/de/topobyte/livecg/algorithms/voronoi/fortune/TestFortunesSweep.java
@@ -17,25 +17,51 @@
  */
 package de.topobyte.livecg.algorithms.voronoi.fortune;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
 
 import de.topobyte.livecg.algorithms.voronoi.fortune.geometry.Point;
 import de.topobyte.livecg.algorithms.voronoi.fortune.ui.swing.FortuneDialog;
+import de.topobyte.livecg.core.geometry.geom.Chain;
+import de.topobyte.livecg.core.geometry.geom.Coordinate;
+import de.topobyte.livecg.core.geometry.geom.Node;
+import de.topobyte.livecg.core.geometry.io.ContentReader;
+import de.topobyte.livecg.geometryeditor.geometryeditor.Content;
 
 public class TestFortunesSweep
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException,
+			ParserConfigurationException, SAXException
 	{
-		Logger.getLogger(Algorithm.class.getPackage().getName()).setLevel(Level.DEBUG);
-		
+		Logger.getLogger(Algorithm.class.getPackage().getName()).setLevel(
+				Level.DEBUG);
+
 		FortuneDialog dialog = new FortuneDialog();
 		Algorithm algorithm = dialog.getAlgorithm();
-		algorithm.addSite(new Point(200, 200));
-		algorithm.addSite(new Point(250, 250));
-		algorithm.addSite(new Point(300, 150));
-		algorithm.addSite(new Point(350, 300));
+
+		// algorithm.addSite(new Point(200, 200));
+		// algorithm.addSite(new Point(250, 250));
+		// algorithm.addSite(new Point(300, 150));
+		// algorithm.addSite(new Point(350, 300));
+
+		String path = "res/presets/voronoi/Points2.geom";
+		ContentReader contentReader = new ContentReader();
+		Content content = contentReader.read(new File(path));
+		for (Chain chain : content.getChains()) {
+			for (int i = 0; i < chain.getNumberOfNodes(); i++) {
+				Node node = chain.getNode(i);
+				Coordinate c = node.getCoordinate();
+				algorithm.addSite(new Point(c.getX(), c.getY()));
+			}
+		}
+
 		algorithm.restart();
 	}
 }
