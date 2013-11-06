3139dd5c8fc213343db036fbb39cbe5845799dbd
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-11-06 11:16:41 +0100

    Removed trailing whitespace

--- project/src/de/topobyte/livecg/util/ImageLoader.java
+++ project/src/de/topobyte/livecg/util/ImageLoader.java
@@ -1,17 +1,17 @@
-/* This file is part of LiveCG. 
- * 
+/* This file is part of LiveCG.$
+ *$
  * Copyright (C) 2013  Sebastian Kuerten
- * 
+ *$
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *$
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *$
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -29,7 +29,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * @author Sebastian Kuerten (sebastian.kuerten@fu-berlin.de)
- * 
+ *$
  */
 public class ImageLoader
 {
@@ -40,7 +40,7 @@ public class ImageLoader
 	 * Load an image from the given filename. The file-resource will be resolved
 	 * by using the classloader. ImageIO will be tried first, then SVG via
 	 * batik.
-	 * 
+	 *$
 	 * @param filename
 	 *            the resource to load.
 	 * @return the Icon loaded or null.
