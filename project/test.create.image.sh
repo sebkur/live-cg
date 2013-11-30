5fad55405088315ce422e10ee57aa59d8c1bcd1a
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-11-30 12:47:43 +0100

    Test script

---
+++ project/test.create.image.sh
@@ -0,0 +1,5 @@
+./scripts/livecg-create-image -input res/presets/polygons/Big.geom -output geometry.png -output_format png -visualization geometry
+./scripts/livecg-create-image -input res/presets/polygons/Big.geom -output dcel.png -output_format png -visualization dcel
+./scripts/livecg-create-image -input res/presets/polygons/Big.geom -output monotone.png -output_format png -visualization monotone
+./scripts/livecg-create-image -input res/presets/polygons/Big.geom -output triangulation.png -output_format png -visualization triangulation
+./scripts/livecg-create-image -input res/presets/polygons/Big.geom -output spip.png -output_format png -visualization spip
