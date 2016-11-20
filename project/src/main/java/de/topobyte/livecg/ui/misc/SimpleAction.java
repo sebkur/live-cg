e1ac167b282ab5293931c7e8b9e7fd3a32dec2fb
Sebastian Kürten <sebastian@topobyte.de>
2016-11-20 16:34:56 +0100

    Base SimpleAction on SimpleAction from swing-utils

--- project/src/main/java/de/topobyte/livecg/ui/misc/SimpleAction.java
+++ project/src/main/java/de/topobyte/livecg/ui/misc/SimpleAction.java
@@ -18,10 +18,6 @@
 
 package de.topobyte.livecg.ui.misc;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.Icon;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,53 +27,17 @@ import de.topobyte.livecg.util.ImageLoader;
  * @author Sebastian Kuerten (sebastian.kuerten@fu-berlin.de)
  * 
  */
-public abstract class SimpleAction extends AbstractAction
+public abstract class SimpleAction extends
+		de.topobyte.swing.util.action.SimpleAction
 {
 
 	private static final long serialVersionUID = 1727617884915345905L;
 
 	final static Logger logger2 = LoggerFactory.getLogger(SimpleAction.class);
 
-	/**
-	 * The name of this action.
-	 */
-	protected String name = null;
-
-	/**
-	 * The short description of this action.
-	 */
-	protected String description = null;
-
-	/**
-	 * The icon for this action.
-	 */
-	protected Icon icon = null;
-
-	/**
-	 * Create a SimpleAction that provides name and description
-	 * 
-	 * @param name
-	 *            the name of the action.
-	 * @param description
-	 *            the short description of the action.
-	 */
 	public SimpleAction(String name, String description)
 	{
-		this.name = name;
-		this.description = description;
-	}
-
-	@Override
-	public Object getValue(String key)
-	{
-		if (key.equals("SmallIcon")) {
-			return icon;
-		} else if (key.equals(Action.NAME)) {
-			return name;
-		} else if (key.equals(Action.SHORT_DESCRIPTION)) {
-			return description;
-		}
-		return null;
+		super(name, description);
 	}
 
 	/**
@@ -90,28 +50,7 @@ public abstract class SimpleAction extends AbstractAction
 	{
 		logger2.debug("loading icon: " + filename);
 
-		icon = ImageLoader.load(filename);
+		setIcon(ImageLoader.load(filename));
 	}
 
-	/**
-	 * Set the name of this action.
-	 * 
-	 * @param name
-	 *            the new name.
-	 */
-	protected void setName(String name)
-	{
-		this.name = name;
-	}
-
-	/**
-	 * Set the description of this action.
-	 * 
-	 * @param description
-	 *            the new description.
-	 */
-	protected void setDescription(String description)
-	{
-		this.description = description;
-	}
 }
